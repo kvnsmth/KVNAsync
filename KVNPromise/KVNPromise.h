faf23535df15c62e0b5a7d6845909d601dad1e69
Kevin Smith <kevin@kevinsmith.cc>
2013-08-15 11:09:11 -0700

    Initial Commit

---
+++ KVNPromise/KVNPromise.h
@@ -0,0 +1,13 @@
+//
+//  KVNPromise.h
+//  KVNPromise
+//
+//  Created by Kevin Smith on 8/15/13.
+//  Copyright (c) 2013 Kevin Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KVNPromise : NSObject
+
+@end
---
+++ KVNPromise/KVNPromise.m
@@ -0,0 +1,13 @@
+//
+//  KVNPromise.m
+//  KVNPromise
+//
+//  Created by Kevin Smith on 8/15/13.
+//  Copyright (c) 2013 Kevin Smith. All rights reserved.
+//
+
+#import "KVNPromise.h"
+
+@implementation KVNPromise
+
+@end
